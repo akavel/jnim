3d2cae2a830166514aec55860e6be444d5ee0079
Mateusz Czapliński <czapkofan@gmail.com>
2019-11-04 23:22:26 +0100

    private/jni_export: start adding test verifying genDexGlue is called

---
+++ tests/test_jni_export_dex.nim
@@ -0,0 +1,62 @@
+
+when defined jnimGenDex:
+  import ../jnim/private / [ jni_api, jni_generator, jni_export ],
+         ./common,
+         unittest
+
+  echo "hello jniExport"
+
+  jclass io.github.yglukhov.jnim.ExportTestClass$Interface of JVMObject:
+    proc voidMethod()
+    proc intMethod*(): jint # Test public
+    proc stringMethod(): string
+    proc stringMethodWithArgs(s: string, i: jint): string
+
+  jclass io.github.yglukhov.jnim.ExportTestClass$Tester of JVMObject:
+    proc new
+    proc callVoidMethod(r: Interface)
+    proc callIntMethod(r: Interface): jint
+    proc callStringMethod(r: Interface): string
+    proc callStringMethodWithArgs(r: Interface, s: string, i: jint): string
+
+  jclass io.github.yglukhov.jnim.ExportTestClass$Implementation of Interface:
+    proc new
+
+  type
+    MyObjData = ref object
+      a: int
+
+    MyObj = ref object of JVMObject
+      data: MyObjData
+
+    MyObjSub = ref object of MyObj
+    ImplementationSub = ref object of Implementation
+
+  jexport MyObj implements Interface:
+    proc new() = super()
+
+    proc voidMethod() # Test fwd declaration
+
+    proc intMethod*(): jint = # Test public
+      return 123
+
+    proc stringMethod(): string =
+      return "Hello world"
+
+    proc stringMethodWithArgs(s: string, i: jint): string =
+      return "123" & $i & s
+
+  jexport MyObjSub extends MyObj:
+    proc new = super()
+
+    proc stringMethod(): string =
+      "Nim"
+
+  jexport ImplementationSub extends Implementation:
+    proc new() = super()
+
+    proc stringMethod(): string =
+      this.super.stringMethod() & " is awesome"
+
+  proc voidMethod(this: MyObj) =
+    inc this.data.a
